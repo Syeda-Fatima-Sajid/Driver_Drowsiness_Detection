ba4e64edab8f606091c1b6d7b92c8c63ab823ba3
Syeda Fatima Sajid <148909339+Syeda-Fatima-Sajid@users.noreply.github.com>
2026-06-09 12:40:09 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
 import tensorflow as tf
 
-st.write("TensorFlow version:", tf.__version__)
+st.title("TFLite Test")
+
+interpreter = tf.lite.Interpreter(
+    model_path="drowsiness_model_fixed.tflite"
+)
+
+interpreter.allocate_tensors()
+
+st.success("✅ Model Loaded Successfully")
