113f7e46df2da07dbf91c04068b1dd157e019623
Syeda Fatima Sajid <148909339+Syeda-Fatima-Sajid@users.noreply.github.com>
2026-06-09 12:26:49 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,4 @@
 import streamlit as st
 import tensorflow as tf
-import os
 
-st.title("TFLite Debug")
-
-st.write("File exists:", os.path.exists("drowsiness_model.tflite"))
-st.write("File size (MB):", os.path.getsize("drowsiness_model.tflite") / (1024 * 1024))
-
-try:
-    interpreter = tf.lite.Interpreter(
-        model_path="drowsiness_model.tflite"
-    )
-
-    interpreter.allocate_tensors()
-
-    st.success("✅ TFLite loaded successfully!")
-
-except Exception as e:
-    st.error(str(e))
+st.write("TensorFlow version:", tf.__version__)
